a5493c4174bff3dce32a71958a969899061bf830
nigraforce <aleshka2012@gmail.com>
2019-09-08 03:28:31 +0300

    теперь html-файлы подгружают правильные css-ки и прочую вложенную статику

--- server.js
+++ server.js
@@ -2,27 +2,34 @@ const http = require('http');
 const fs = require('fs');
 const { join } = require('path');
 
+const url = require('url');
+
 const server = http.createServer((req, res) => {
-    let {url} = req;
-    
-    if (/^[^.]+$/.test(url)) {
-        console.log('no dot')
-        url = `${url}${url.endsWith('/') ? '' : '/'}index.html`
+    let currentUrl = url.parse(req.url, true).pathname;
+
+    if (/^[^.]+$/.test(currentUrl)) {
+        if (!currentUrl.endsWith('/')) {
+            res.setHeader("Location", req.url + "/");
+            res.writeHead(302);
+            res.end();
+            return
+        }
+        currentUrl = `${currentUrl}index.html`
     }
 
+    const newPath = join(__dirname, `public${currentUrl}`)
+
     try {
-        body = fs.readFileSync(join(__dirname, `public${url}`)).toString('utf8');
+        body = fs.readFileSync(newPath)
     } catch (e) {
         res.statusCode = 404;
         res.end();
 
         return;
     }
-
     res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(body)
 
-    res.end();
+    res.end(body);
 }).listen(3000);
 
 module.exports = server
