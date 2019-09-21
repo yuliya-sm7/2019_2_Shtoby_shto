7fe695e3c5fb2f58d26c4b6bc44d036b1d25056d
yuliya <yuliya.sm7@gmail.com>
2019-09-21 21:00:12 +0300

    T2 подготовка форм

--- public/components/Login/login.js
+++ public/components/Login/login.js
@@ -1,22 +1,30 @@
 export function reg() {
-    application.innerHTML = `
-<div class="cont">
+    const application = document.getElementById('application');
+    application.innerHTML = '';
+    const cont  = document.createElement('div');
+    cont.className = 'cont';
+    application.appendChild(cont);
+
+    const signin = ` 
     <div class="form sign-in">
         <h2>С возвращением,</h2>
-        <label>
-            <span>Email</span>
-            <input type="email"/>
-        </label>
-        <label>
-            <span>Password</span>
-            <input type="password"/>
-        </label>
+        <form id="in">
+            <label>
+                <span>Email</span>
+                <input name="email" type="email"/>
+            </label>
+            <label>
+                <span>Password</span>
+                <input name="password" type="password"/>
+            </label>
+        </form>
         <p class="forgot-pass">Забыли пароль? Ничем не можем вам помочь</p>
-        <button type="button" class="submit">Войти</button>
-        <button type="button" class="vk-btn">Войти через <span>ВКонтакте</span></button>
-    </div>
-    <div class="sub-cont">
-        <div class="img">
+        <button type="submit" form="in">Войти</button>
+        <button class="vk-btn">Войти через <span>ВКонтакте</span></button>
+    </div>`;
+
+    const image = `
+        <div class="bmstu">
             <div class="img__text m--up">
                 <h2>Первый раз?</h2>
                 <p>Зарегистрируйтесь и откройте для себя множество новых возможностей!</p>
@@ -29,27 +37,64 @@ export function reg() {
                 <span class="m--up">Регистрация</span>
                 <span class="m--in">Вход</span>
             </div>
-        </div>
-        <div class="form sign-up">
-            <h2>Время почувствовать себя как дома,</h2>
+        </div>`;
+
+    const signup = `
+    <div class="form sign-up">
+        <h2>Время почувствовать себя как дома,</h2>
+        <form id="up">
             <label>
                 <span>Ваше имя</span>
-                <input type="text"/>
+                <input name="name" type="text"/>
             </label>
             <label>
                 <span>Email</span>
-                <input type="email"/>
+                <input name="email" type="email"/>
             </label>
             <label>
                 <span>Password</span>
-                <input type="password"/>
+                <input name="password" type="password"/>
             </label>
-            <button type="button" class="submit">Зарегистрироваться</button>
-            <button type="button" class="vk-btn">Связать с аккаунтом <span>ВКонтакте</span></button>
-        </div>
-    </div>
-</div>
-`;
+        </form>
+        <button type="submit" form="up">Зарегистрироваться</button>
+        <button class="vk-btn">Связать с аккаунтом <span>ВКонтакте</span></button>
+    </div>`;
+
+    cont.innerHTML = signin + `<div class="sub-cont">` + image + signup + `</div>`;
+
+    const form = document.getElementById('in');
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const email = form.elements['email'].value;
+        const password = form.elements['password'].value;
+        alert("Ждем когда запилится сервер");
+/*
+        AjaxModule.doPost({
+            url: '/signup',
+            body: {email, password},
+            callback(status, responseText) {
+                if (status === 201) {
+                    createProfile();
+                    return;
+                }
+
+                const {error} = JSON.parse(responseText);
+                alert(error);
+            }
+        });
+ */
+    });
+
+    const form2 = document.getElementById('up');
+    form2.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const email = form2.elements['email'].value;
+        const password = form2.elements['password'].value;
+        const name = form2.elements['name'].value;
+        alert("Ждем когда запилится сервер");
+    });
+
+// украшение
     document.querySelector('.img__btn').addEventListener('click', function () {
         document.querySelector('.cont').classList.toggle('s--signup');
     });
--- public/main.js
+++ public/main.js
@@ -4,14 +4,28 @@ import {
 
 const application = document.getElementById('application');
 
+const col = document.createElement("div");
+col.innerHTML = `
+<div class="row">
+    <div class="column" id="col1"></div>
+    <div class="column" id="col2"></div>
+</div>`;
+
+application.appendChild(col);
+const left = document.getElementById('col1');
+const right = document.getElementById('col2');
+
 const login = document.createElement('button');
-login.textContent = 'Залогинься!';
+login.textContent = 'Залогинься и все увидишь';
 login.type = 'button';
-login.className = 'submit';
 login.addEventListener('click', reg);
 
 const screen = document.createElement('img');
 screen.src = "hero-a.svg";
 
-application.appendChild(login);
-application.appendChild(screen);
+const description = document.createElement("div");
+description.innerHTML = '<h1>Мы создали</h1> <h1>Чтобы что</h1>';
+
+left.appendChild(screen);
+right.appendChild(description);
+right.appendChild(login);
