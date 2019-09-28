0ee1cdb1889741e15a2cb10c6477971b4b5ec061
yuliya <yuliya.sm7@gmail.com>
2019-09-28 12:10:56 +0300

    T2 разлогиниться

--- public/components/Board/board.js
+++ public/components/Board/board.js
@@ -1,9 +1,17 @@
+import {doPost} from "../../modules/ajax";
+import {main} from "../../main";
 const template = require('./board.pug');
+
 /**
  * Генерирует Канбан-доску
  */
 export function createBoard() {
-  const html = template();
-  const application = document.getElementById('application');
-  application.innerHTML = html;
+    const html = template();
+    const application = document.getElementById('application');
+    application.innerHTML = html;
+    const logout = document.getElementById('logout');
+    logout.addEventListener('click', () => {
+      doPost('/logout', {})
+          .then(() => main())
+    })
 }
--- public/main.js
+++ public/main.js
@@ -1,11 +1,14 @@
 import {reg} from './components/Login/login.js';
 import {viewPUG} from './components/PUG/viewPug.js';
-
 const template = require('./main.pug');
-const application = document.getElementById('application');
-const html = template();
-application.innerHTML = html;
-const login = document.getElementById('login');
-login.addEventListener('click', reg);
-const mops = document.getElementById('mops');
-mops.addEventListener('click', viewPUG);
+
+export function main() {
+    const application = document.getElementById('application');
+    const html = template();
+    application.innerHTML = html;
+    const login = document.getElementById('login');
+    login.addEventListener('click', reg);
+    const mops = document.getElementById('mops');
+    mops.addEventListener('click', viewPUG);
+}
+main();
