01ae6194a1ea1f13ebbe8d13022ac9532be9dcc7
nigraforce <aleshka2012@gmail.com>
2019-09-28 20:58:49 +0300

    removed unneded stuff

--- public/script.js
+++
@@ -1,9 +0,0 @@
-const title = document.getElementsByTagName('h1')[0];
-const button = document.getElementById('butt1');
-
-var state = true
-
-button.addEventListener('click', () => {
-    title.innerText = state ? 'goodbye' : "HELLO";
-    state = !state;
-});
