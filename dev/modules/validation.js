8b2256eebb404be1978ece6ccb72ef4e2ecb6a1a
nigraforce <aleshka2012@gmail.com>
2019-09-30 01:51:16 +0300

    вынес регулярные выражения в константы

--- dev/modules/validation.js
+++ dev/modules/validation.js
@@ -1,10 +1,13 @@
+const nameRegexp = /^[a-zA-Z][a-zA-Z0-9_.,-]{4,14}$/;
+const emailRegexp = /^([a-z0-9_\\-]+\.)*[a-z0-9_\\-]+@([a-z0-9][a-z0-9\\-]*[a-z0-9]\.)+[a-z]{2,4}$/i
+
 /**
  * Проверяет коректность ника
  * @param {String} nickname
  * @return {boolean} validate
  */
 export function checkName(nickname = '') {
-  return nickname.match(/^[a-zA-Z][a-zA-Z0-9_.,-]{4,14}$/);
+  return nickname.match(nameRegexp);
 }
 
 /**
@@ -13,9 +16,7 @@ export function checkName(nickname = '') {
  * @return {boolean} validate
  */
 export function checkEmail(email = '') {
-  return email.match(
-      /^([a-z0-9_\\-]+\.)*[a-z0-9_\\-]+@([a-z0-9][a-z0-9\\-]*[a-z0-9]\.)+[a-z]{2,4}$/i,
-  );
+  return email.match(emailRegexp);
 }
 
 /**
