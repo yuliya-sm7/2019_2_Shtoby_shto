1bced0577e94605024a0e1bbe1dd8d975dcd3d8e
yuliya <yuliya.sm7@gmail.com>
2019-09-29 15:29:15 +0300

    стиль шапки добавлен

--- public/server.js
+++ public/server.js
@@ -1,47 +1,47 @@
 const http = require('http');
 const fs = require('fs');
-const { join } = require('path');
+const {join} = require('path');
 
 const url = require('url');
 
 const filetypes = {
-    "html": "text/html",
-    "css" : "text/css",
-    "js"  : "application/javascript",
-    "svg" : "image/svg+xml"
+  'html': 'text/html',
+  'css': 'text/css',
+  'js': 'application/javascript',
+  'svg': 'image/svg+xml',
 };
 
 const server = http.createServer((req, res) => {
-    let currentUrl = url.parse(req.url, true).pathname;
-    
-    if (/^[^.]+$/.test(currentUrl)) {
-        if (!currentUrl.endsWith('/')) {
-            res.setHeader("Location", req.url + "/");
-            res.writeHead(302);
-            res.end();
-            return
-        }
-        currentUrl = `${currentUrl}index.html`
-        res.setHeader('Content-Type', `${filetypes['html']}`);
-    } else {
-        const ft = `${currentUrl.split('.')[1]}`;
-        res.setHeader('Content-Type', `${filetypes[ft]}`);
+  let currentUrl = url.parse(req.url, true).pathname;
+
+  if (/^[^.]+$/.test(currentUrl)) {
+    if (!currentUrl.endsWith('/')) {
+      res.setHeader('Location', req.url + '/');
+      res.writeHead(302);
+      res.end();
+      return;
     }
+    currentUrl = `${currentUrl}index.html`;
+    res.setHeader('Content-Type', `${filetypes['html']}`);
+  } else {
+    const ft = `${currentUrl.split('.')[1]}`;
+    res.setHeader('Content-Type', `${filetypes[ft]}`);
+  }
 
-    const newPath = join(__dirname, `${currentUrl}`)
+  const newPath = join(__dirname, `${currentUrl}`);
 
-    try {
-        body = fs.readFileSync(newPath)
-    } catch (e) {
-        res.statusCode = 404;
-        res.end();
+  try {
+    body = fs.readFileSync(newPath);
+  } catch (e) {
+    res.statusCode = 404;
+    res.end();
 
-        return;
-    }
-    // res.writeHead(200, {'Content-Type': 'text/html'})
-    res.writeHead(200);
+    return;
+  }
+  // res.writeHead(200, {'Content-Type': 'text/html'})
+  res.writeHead(200);
 
-    res.end(body);
+  res.end(body);
 }).listen(3000);
 
-module.exports = server
+module.exports = server;
